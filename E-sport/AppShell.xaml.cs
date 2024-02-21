2843b75e237ad4af28d005ecf7ba7d84ff9b23de
Yoyo <michon.yoann@outlook.com>
2024-02-21 13:45:50 +0100

    initialize

---
+++ E-sport/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace E_sport
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ E-sport/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace E_sport
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
